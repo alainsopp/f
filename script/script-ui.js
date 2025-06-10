51e9f0e9239b730976544cad2a8118fcd8ce3f84
kutor <n.catin@protonmail.com>
2025-06-11 00:25:56 +0200

    Adds categories to sankey chart

--- script/script-ui.js
+++ script/script-ui.js
@@ -1,3 +1,46 @@
+
+var ops = operations.split("\n")
+
+var category_map = {
+  'Alimentation & restaurants'  : 0,
+  'Achats & shopping'           : 1,
+  'Loisirs'                     : 2,
+  'Logement & charges'          : 3,
+  'Sante'                       : 4,
+  'Don'                         : 5,
+  'Impôts, taxes & frais'       : 6,
+  'Transports'                  : 7,
+  'Voyages'                     : 8,
+  'Epargne & placements'        : 9  
+}
+
+// Martix of month (Y axis) by Categories (X axis)
+var ops_cube = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+]
+
+// Adds up expenses by category for each month
+for (var i=1;i<ops.length;i++) {
+  op = ops[i].split(";")
+  month = Number(op[0].substring(3,5))-1
+  categ = op[1]
+  amount = op[4]  
+  if(op[4] != '') {    
+    ops_cube[month][category_map[categ]]+= Math.round(amount)
+  }  
+}
+
 window.addEventListener('load', 
   function () {
     period_buttons = document.getElementsByClassName('period-item')
--- script/script.js
+++ script/script.js
@@ -2,16 +2,31 @@ anychart.onDocumentReady(function () {
     var data = [
       {from: 'Wages', to: 'Budget', value: 190},
       {from: 'Reimboursments', to: 'Budget', value: 15},      
-      {from: 'Budget', to: 'Food & restaurants', value: 6},      
-      {from: 'Budget', to: 'Purchases', value: 2},
-      {from: 'Budget', to: 'Hobbies', value: 7},
-      {from: 'Budget', to: 'Housing and charges', value: 0},
-      {from: 'Budget', to: 'Health', value: 31},
-      {from: 'Budget', to: 'Donation', value: 26},
-      {from: 'Budget', to: 'Taxes & fees', value: 62},
-      {from: 'Budget', to: 'Transportation', value: 5},
-      {from: 'Budget', to: 'Travel', value: 4},
-      {from: 'Budget', to: 'Savings & investments', value: 6}      
+      {from: 'Budget', to: 'Food & restaurants', value: ops_cube[0][0]},      
+      {from: 'Budget', to: 'Purchases', value: ops_cube[0][1]},
+      {from: 'Budget', to: 'Hobbies', value: ops_cube[0][2]},
+      {from: 'Budget', to: 'Housing and charges', value: ops_cube[0][3]},
+      {from: 'Budget', to: 'Health', value: ops_cube[0][4]},
+      {from: 'Budget', to: 'Donation', value: ops_cube[0][5]},
+      {from: 'Budget', to: 'Taxes & fees', value: ops_cube[0][6]},
+      {from: 'Budget', to: 'Transportation', value: ops_cube[0][7]},
+      {from: 'Budget', to: 'Travel', value: ops_cube[0][8]},
+      {from: 'Budget', to: 'Savings & investments', value: ops_cube[0][9]}      
+    ];
+
+    var data = [
+      //{from: 'Wages', to: 'Budget', value: 190},
+      //{from: 'Reimboursments', to: 'Budget', value: 15},      
+      {from: 'Budget', to: 'Alimentation & restaurants', value: ops_cube[0][0]},      
+      {from: 'Budget', to: 'Achats & shopping', value: ops_cube[0][1]},
+      {from: 'Budget', to: 'Loisirs', value: ops_cube[0][2]},
+      {from: 'Budget', to: 'Logement & charges', value: ops_cube[0][3]},
+      {from: 'Budget', to: 'Sante', value: ops_cube[0][4]},
+      {from: 'Budget', to: 'Don', value: ops_cube[0][5]},
+      {from: 'Budget', to: 'Impôts, taxes & frais', value: ops_cube[0][6]},      
+      {from: 'Budget', to: 'Transports', value: ops_cube[0][7]},
+      {from: 'Budget', to: 'Voyages', value: ops_cube[0][8]},
+      {from: 'Budget', to: 'Epargne & placements', value: ops_cube[0][9]}
     ];
 
     // create a sankey diagram instance
@@ -48,7 +63,7 @@ anychart.onDocumentReady(function () {
     chart
       .node()
       .labels()
-      .format('<span style="font-weight:bold">{%name}</span><br>{%value}');
+      .format('<span style="font-weight:bold">{%name}</span> {%value}');
 
     // customize the links
     chart.flow({
